64833fe01d03148c320452b5e153e267444397cb
romendezs <ro.mendez005@gmail.com>
2024-09-02 09:48:28 -0600

    Guia 3 Ejercicio 5

---
+++ MS24030_Guia3_Ej5/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace MS24030_Guia3_Ej5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtConsumo = new System.Windows.Forms.TextBox();
+            this.btnCobrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Restaturantes \"Buen Provecho\"";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingrese el consumo:";
+            // 
+            // txtConsumo
+            // 
+            this.txtConsumo.Location = new System.Drawing.Point(63, 114);
+            this.txtConsumo.Name = "txtConsumo";
+            this.txtConsumo.Size = new System.Drawing.Size(100, 22);
+            this.txtConsumo.TabIndex = 2;
+            // 
+            // btnCobrar
+            // 
+            this.btnCobrar.Location = new System.Drawing.Point(266, 114);
+            this.btnCobrar.Name = "btnCobrar";
+            this.btnCobrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCobrar.TabIndex = 3;
+            this.btnCobrar.Text = "Cobrar";
+            this.btnCobrar.UseVisualStyleBackColor = true;
+            this.btnCobrar.Click += new System.EventHandler(this.btnCobrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 203);
+            this.Controls.Add(this.btnCobrar);
+            this.Controls.Add(this.txtConsumo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtConsumo;
+        private System.Windows.Forms.Button btnCobrar;
+    }
+}
+
---
+++ MS24030_Guia3_Ej5/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MS24030_Guia3_Ej5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCobrar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtConsumo.Text == "")
+                {
+                    throw new Exception("Los campos no peuden estar vacios");
+                }
+
+                Factura factura = new Factura();
+
+                factura.Monto = Convert.ToDouble(txtConsumo.Text);
+                factura.calcularDescuento();
+
+                if (factura.Descuento == 0)
+                {
+                    MessageBox.Show($"Total:{factura.Total} \nPasar a Caja por un cupón Promocional");
+                }else
+                {
+                    MessageBox.Show($"Total: ${factura.Total} \nDescuento aplicado (25%): ${factura.Descontar}");
+                }
+
+
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class Factura
+    {
+        private double monto;
+        private int descuento;
+
+        private double total;
+        private double descontar;
+
+        public double Monto { 
+            get => monto; 
+            set { if (value != null) { monto = value; } } 
+        }
+        public int Descuento
+        {
+            get => descuento;
+            set {  descuento = value; }
+        }
+
+        public double Total { get => total; set => total = value; }
+        public double Descontar { get => descontar; set => descontar = value; }
+
+        public int calcularDescuento()
+        {
+            if (this.Monto > 30)
+            {
+                this.Descuento = 25;
+                this.Descontar = this.Monto * 0.25;
+                this.Total = this.Monto - this.Descontar;
+            }
+            else
+            {
+                this.Descuento = 0;
+                this.Total = this.Monto;
+            };
+
+            return descuento;
+        }
+    }
+}
