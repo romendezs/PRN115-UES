2ecd12994b2974dc627fb6b8891c11235e9177f7
romendezs <ro.mendez005@gmail.com>
2024-09-06 12:19:04 -0600

    restructured files

---
+++ Guia 3/MS24030_Guia3_Ej7/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace MS24030_Guia3_Ej7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHoras = new System.Windows.Forms.TextBox();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(241, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese el numero de horas trabajadas:";
+            // 
+            // txtHoras
+            // 
+            this.txtHoras.Location = new System.Drawing.Point(50, 77);
+            this.txtHoras.Name = "txtHoras";
+            this.txtHoras.Size = new System.Drawing.Size(100, 22);
+            this.txtHoras.TabIndex = 1;
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(50, 116);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(75, 23);
+            this.btnEnviar.TabIndex = 2;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 192);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.txtHoras);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHoras;
+        private System.Windows.Forms.Button btnEnviar;
+    }
+}
+
---
+++ Guia 3/MS24030_Guia3_Ej7/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MS24030_Guia3_Ej7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                if (txtHoras.Text == "")
+                {
+                    throw new Exception("El campo no puede estar vacio");
+                }else if(Convert.ToDouble(txtHoras.Text) <= 0)
+                {
+                    throw new Exception("El valor debe ser mayor que cero");
+                }
+
+                Trabajador obrero = new Trabajador();
+                obrero.Horas = Convert.ToDouble(txtHoras.Text);
+                obrero.Salario();
+
+                MessageBox.Show($"El salario del trabjador es de ${obrero.getSalario}");
+
+
+
+            }catch(Exception ex){ 
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class Trabajador
+    {
+        private double horas;
+        private double salario;
+
+        public double Horas { get => horas; set => horas = value; }
+        public double getSalario { get => salario; }
+
+        public void Salario() {
+            if (this.Horas<=40)
+            {
+                salario = this.Horas*16;
+            }
+            else{
+
+                salario = (40 * 16) + ((this.Horas - 40) * 20);
+            }
+        }
+
+        
+    }
+}
