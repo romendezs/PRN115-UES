49f602def6866c3688d6bdc9cb9ec4aa755470c4
romendezs <ro.mendez005@gmail.com>
2024-09-11 10:01:49 -0600

    Guia 3 Parte II Ejercicio 1

---
+++ Guia 3 PII/MS24030_Guia3_Ej1/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace MS24030_Guia3_Ej1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtRepuesto = new System.Windows.Forms.TextBox();
+            this.nudPrecio = new System.Windows.Forms.NumericUpDown();
+            this.nudManoObra = new System.Windows.Forms.NumericUpDown();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnTotal = new System.Windows.Forms.Button();
+            this.dgvFactura = new System.Windows.Forms.DataGridView();
+            this.Repuesto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Precio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ManoObra = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lblTotal = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPrecio)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudManoObra)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFactura)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(45, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Repuesto";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(224, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Precio";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(407, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(162, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Mano de obra";
+            // 
+            // txtRepuesto
+            // 
+            this.txtRepuesto.Location = new System.Drawing.Point(50, 88);
+            this.txtRepuesto.Name = "txtRepuesto";
+            this.txtRepuesto.Size = new System.Drawing.Size(100, 22);
+            this.txtRepuesto.TabIndex = 3;
+            // 
+            // nudPrecio
+            // 
+            this.nudPrecio.Location = new System.Drawing.Point(229, 89);
+            this.nudPrecio.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudPrecio.Name = "nudPrecio";
+            this.nudPrecio.Size = new System.Drawing.Size(102, 22);
+            this.nudPrecio.TabIndex = 4;
+            this.nudPrecio.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // nudManoObra
+            // 
+            this.nudManoObra.Location = new System.Drawing.Point(412, 89);
+            this.nudManoObra.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nudManoObra.Name = "nudManoObra";
+            this.nudManoObra.Size = new System.Drawing.Size(102, 22);
+            this.nudManoObra.TabIndex = 5;
+            this.nudManoObra.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(632, 45);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(122, 23);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "Añadir Repuesto";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnTotal
+            // 
+            this.btnTotal.Location = new System.Drawing.Point(632, 74);
+            this.btnTotal.Name = "btnTotal";
+            this.btnTotal.Size = new System.Drawing.Size(122, 23);
+            this.btnTotal.TabIndex = 7;
+            this.btnTotal.Text = "Calcular Monto";
+            this.btnTotal.UseVisualStyleBackColor = true;
+            this.btnTotal.Click += new System.EventHandler(this.btnTotal_Click);
+            // 
+            // dgvFactura
+            // 
+            this.dgvFactura.AllowUserToAddRows = false;
+            this.dgvFactura.AllowUserToDeleteRows = false;
+            this.dgvFactura.AllowUserToResizeColumns = false;
+            this.dgvFactura.AllowUserToResizeRows = false;
+            this.dgvFactura.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFactura.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Repuesto,
+            this.Precio,
+            this.ManoObra});
+            this.dgvFactura.Location = new System.Drawing.Point(80, 143);
+            this.dgvFactura.Name = "dgvFactura";
+            this.dgvFactura.RowHeadersWidth = 51;
+            this.dgvFactura.RowTemplate.Height = 24;
+            this.dgvFactura.Size = new System.Drawing.Size(602, 170);
+            this.dgvFactura.TabIndex = 8;
+            // 
+            // Repuesto
+            // 
+            this.Repuesto.HeaderText = "Repuesto";
+            this.Repuesto.MinimumWidth = 6;
+            this.Repuesto.Name = "Repuesto";
+            this.Repuesto.Width = 300;
+            // 
+            // Precio
+            // 
+            this.Precio.HeaderText = "Precio";
+            this.Precio.MinimumWidth = 6;
+            this.Precio.Name = "Precio";
+            this.Precio.Width = 125;
+            // 
+            // ManoObra
+            // 
+            this.ManoObra.HeaderText = "Mano de obra";
+            this.ManoObra.MinimumWidth = 6;
+            this.ManoObra.Name = "ManoObra";
+            this.ManoObra.Width = 125;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(248, 337);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(41, 16);
+            this.lblTotal.TabIndex = 9;
+            this.lblTotal.Text = "Total:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.dgvFactura);
+            this.Controls.Add(this.btnTotal);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.nudManoObra);
+            this.Controls.Add(this.nudPrecio);
+            this.Controls.Add(this.txtRepuesto);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudPrecio)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudManoObra)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFactura)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtRepuesto;
+        private System.Windows.Forms.NumericUpDown nudPrecio;
+        private System.Windows.Forms.NumericUpDown nudManoObra;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnTotal;
+        private System.Windows.Forms.DataGridView dgvFactura;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Repuesto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Precio;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ManoObra;
+        private System.Windows.Forms.Label lblTotal;
+    }
+}
+
---
+++ Guia 3 PII/MS24030_Guia3_Ej1/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MS24030_Guia3_Ej1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Regex validarProducto = new Regex("^[a-zA-Z]+$");
+            
+            if (validarProducto.IsMatch(txtRepuesto.Text))
+            {
+                //Creating new Repuesto and setting props
+                Repuesto nuevoRepuesto = new Repuesto();
+                nuevoRepuesto.Producto = txtRepuesto.Text;
+                nuevoRepuesto.Precio = Convert.ToDouble(nudPrecio.Text);
+                nuevoRepuesto.ManoObra = Convert.ToDouble(nudManoObra.Text);
+
+                //Adding in the DataGrid
+                DataGridViewRow fila = new DataGridViewRow();
+                fila.CreateCells(dgvFactura);
+
+                fila.Cells[0].Value = nuevoRepuesto.Producto;
+                fila.Cells[1].Value = nuevoRepuesto.Precio.ToString();
+                fila.Cells[2].Value = nuevoRepuesto.ManoObra.ToString();
+
+                dgvFactura.Rows.Add(fila);
+
+            }
+            else
+            {
+                MessageBox.Show("Debe completar correctamente el campo de Producto", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnTotal_Click(object sender, EventArgs e)
+        {
+            double total =0;
+            
+            //Summ of the monto
+            for( int i = 0; i< dgvFactura.Rows.Count; i++)
+            {
+                if (!dgvFactura.Rows[i].IsNewRow)
+                {
+                    total += Convert.ToDouble(dgvFactura.Rows[i].Cells[1].Value) + Convert.ToDouble(dgvFactura.Rows[i].Cells[2].Value);
+                }
+            }
+
+            lblTotal.Text = $"Total: {total.ToString()}";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class Repuesto
+    {
+        private string producto;
+        private double precio;
+        private double manoObra;
+
+        public string Producto { get => producto; set => producto = value; }
+        public double Precio { get => precio; set => precio = value; }
+        public double ManoObra { get => manoObra; set => manoObra = value; }
+
+    }
+}
