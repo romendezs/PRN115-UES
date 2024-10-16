8870966b45be7e0262c96775840b11eef598d7d7
romendezs <ro.mendez005@gmail.com>
2024-10-16 00:12:18 -0600

    Guia 4 PI Ej2

---
+++ Guia 4 PI/Guia4_P1_Ej2/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Guia4_P1_Ej2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSalario = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtYears = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtHoras = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnRecibo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Salario por hora";
+            // 
+            // txtSalario
+            // 
+            this.txtSalario.Location = new System.Drawing.Point(34, 69);
+            this.txtSalario.Name = "txtSalario";
+            this.txtSalario.Size = new System.Drawing.Size(401, 22);
+            this.txtSalario.TabIndex = 1;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(34, 146);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(401, 22);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre empleado";
+            // 
+            // txtYears
+            // 
+            this.txtYears.Location = new System.Drawing.Point(34, 226);
+            this.txtYears.Name = "txtYears";
+            this.txtYears.Size = new System.Drawing.Size(401, 22);
+            this.txtYears.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 194);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Años trabajando";
+            // 
+            // txtHoras
+            // 
+            this.txtHoras.Location = new System.Drawing.Point(34, 301);
+            this.txtHoras.Name = "txtHoras";
+            this.txtHoras.Size = new System.Drawing.Size(401, 22);
+            this.txtHoras.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 269);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Horas mensuales";
+            // 
+            // btnRecibo
+            // 
+            this.btnRecibo.Location = new System.Drawing.Point(161, 359);
+            this.btnRecibo.Name = "btnRecibo";
+            this.btnRecibo.Size = new System.Drawing.Size(141, 23);
+            this.btnRecibo.TabIndex = 8;
+            this.btnRecibo.Text = "Generar recibo";
+            this.btnRecibo.UseVisualStyleBackColor = true;
+            this.btnRecibo.Click += new System.EventHandler(this.btnRecibo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(479, 450);
+            this.Controls.Add(this.btnRecibo);
+            this.Controls.Add(this.txtHoras);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtYears);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSalario);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSalario;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtYears;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtHoras;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnRecibo;
+    }
+}
+
---
+++ Guia 4 PI/Guia4_P1_Ej2/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Guia4_P1_Ej2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRecibo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                Regex validarInt = new Regex("^\\d+$");
+                Regex validarHoras = new Regex("^[1-9]+\\d*$");
+
+                Regex validarDouble = new Regex("^[1-9]+(.\\d+)?$");
+                Regex validarNombre = new Regex("^\\w+(\\s\\w+){0,3}$");
+
+                if(!validarHoras.IsMatch(txtHoras.Text) || !validarInt.IsMatch(txtYears.Text) || !validarDouble.IsMatch(txtSalario.Text) || !validarNombre.IsMatch(txtNombre.Text))
+                {
+
+                    //Handling errors
+                    string msg = "";
+                    if (!validarHoras.IsMatch(txtHoras.Text)) {
+                        msg += "\nRevise el campo de Horas, solo puede ingresar enteros mayores a cero";
+
+                    } if (!validarInt.IsMatch(txtYears.Text)) {
+                        msg += "\nRevise el campo de Años trabajando, solo puede ingresar enteros positivos o cero";
+
+                    } if (!validarDouble.IsMatch(txtSalario.Text)){
+                        msg += ("\nRevise el campo de Salario por hora, solo puede ingresar numeros positivos");
+
+                    }
+                    if (!validarNombre.IsMatch(txtNombre.Text))
+                    {
+                        msg += ("\nRevise el campo de Nombre empleado, solo puede ingresar cuatro palabras");
+                    }
+
+                    throw new Exception(msg);
+                
+                }
+                else
+                {
+
+                    //Creating new empleado
+                    Empleado empleado = new Empleado(
+                        Convert.ToDouble(txtSalario.Text), 
+                        txtNombre.Text, 
+                        Convert.ToInt32(txtYears.Text),
+                        Convert.ToInt32(txtHoras.Text)
+                        );
+
+                    empleado.CalcularCobro();
+
+                    //Displaying results
+                    string result = $"Nombre: {empleado.Nombre}" +
+                        $"\nAntigüedad: {empleado.Antiguo}" +
+                        $"\nValor Hora: ${empleado.Salario}" +
+                        $"\nTotal a Cobrar en Bruto: ${empleado.CobroBruto}" +
+                        $"\nTotal descuentos: ${empleado.Deducciones}" +
+                        $"\nTotal a cobrar en Neto: ${empleado.CobroNeto}";
+
+                    MessageBox.Show(result, "Resultado", MessageBoxButtons.OK);
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,"Error",MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        public class Empleado
+        {
+            private double salario;
+            private string nombre;
+            private int horas;
+            private int antiguo;
+            private double cobroBruto;
+            private double deducciones;
+            private double cobroNeto;
+
+            public double Salario { get => salario; set => salario = value; }
+            public string Nombre { get => nombre; set => nombre = value; }
+            public int Antiguo { get => antiguo; set => antiguo = value; }
+            public double CobroBruto { get => cobroBruto; set => cobroBruto = value; }
+            public double Deducciones { get => deducciones; set => deducciones = value; }
+            public double CobroNeto { get => cobroNeto; set => cobroNeto = value; }
+            public int Horas { get => horas; set => horas = value; }
+
+            public Empleado(double salario, string nombre, int antiguo, int horas)
+            {
+                Salario = salario;
+                Nombre = nombre;
+                Antiguo = antiguo;
+                Horas = horas;
+            }
+
+            public void CalcularCobro()
+            {
+                CobroBruto = (salario * horas) + (antiguo * 30);
+                Deducciones = cobroBruto * 0.13;
+                CobroNeto = cobroBruto - Deducciones;
+            }
+
+        }
+    }
+}
