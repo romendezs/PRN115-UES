ed8bfb976bb9842b4e9ef846aeacc87d54c46273
romendezs <ro.mendez005@gmail.com>
2024-10-15 22:59:31 -0600

    Guia 4 PI Ej1

---
+++ Guia 4 PI/Guia 4 PI/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Guia_4_PI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUnidades = new System.Windows.Forms.TextBox();
+            this.btnPago = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Unidades:";
+            // 
+            // txtUnidades
+            // 
+            this.txtUnidades.Location = new System.Drawing.Point(91, 69);
+            this.txtUnidades.Name = "txtUnidades";
+            this.txtUnidades.Size = new System.Drawing.Size(100, 22);
+            this.txtUnidades.TabIndex = 1;
+            // 
+            // btnPago
+            // 
+            this.btnPago.Location = new System.Drawing.Point(63, 122);
+            this.btnPago.Name = "btnPago";
+            this.btnPago.Size = new System.Drawing.Size(75, 23);
+            this.btnPago.TabIndex = 2;
+            this.btnPago.Text = "Pago";
+            this.btnPago.UseVisualStyleBackColor = true;
+            this.btnPago.Click += new System.EventHandler(this.btnPago_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(178, 122);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCerrar.TabIndex = 3;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 199);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnPago);
+            this.Controls.Add(this.txtUnidades);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUnidades;
+        private System.Windows.Forms.Button btnPago;
+        private System.Windows.Forms.Button btnCerrar;
+    }
+}
+
---
+++ Guia 4 PI/Guia 4 PI/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Guia_4_PI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPago_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Regex validarInt = new Regex("^[1-9]+\\d*$");
+                Regex validarDouble = new Regex("^[1-9]+\\d*(.\\d*)?$");
+
+                if (!validarInt.IsMatch(txtUnidades.Text))
+                {
+                    throw new Exception("Por favor ingrese un numero mayor a cero");
+                }
+                else
+                {
+                    Compra compra = new Compra();
+                    compra.Unidades = Convert.ToInt32(txtUnidades.Text);
+                    compra.CacularDescuento();
+                    compra.CalcularMonto();
+
+                    string message = $"Total a pagar: ${compra.Monto} \nDescuento aplicado: {compra.Descuento*100}%";
+
+                    MessageBox.Show(message, "Total compra", MessageBoxButtons.OK);
+                }
+            } catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+
+    public class Compra
+    {
+        private const double precioUnitario = 650;
+        private int unidades;
+        private double descuento;
+        private double monto;
+
+        public int Unidades { get => unidades; set => unidades = value; }
+        public double Descuento { get => descuento; }
+        public double Monto { get => monto; set => monto = value; }
+
+        public void CacularDescuento()
+        {
+            if (unidades < 5)
+            {
+                descuento = 0.1;
+            } else if (unidades>=5 && unidades <10) {
+                descuento = 0.2;
+            } else
+            {
+                descuento = 0.4;
+            }
+        }
+
+        public void CalcularMonto()
+        {
+            monto = (precioUnitario * unidades) - (precioUnitario * unidades * descuento);
+        }
+
+    }
+}
